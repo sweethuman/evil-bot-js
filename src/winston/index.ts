2aabb5a32ed55f2fba722b639397dbe3281b3fa9
sweethuman <hackscomicon@yahoo.com>
2019-09-19 23:44:28 +0300

    Customize Winston Log Format

--- src/winston/index.ts
+++ src/winston/index.ts
@@ -1,7 +1,21 @@
-import winston, { silly } from 'winston';
+import winston from 'winston';
 import { VorpalTransport } from './vorpalTransport';
+
+const format = winston.format;
+const customFormat = format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`);
+const levelFormat = format(info => {
+    info.level = `[${info.level}]`;
+    return info;
+});
 
 export const logger = winston.createLogger({
     level: 'silly',
+    format: format.combine(
+        format.timestamp({ format: 'D/M/YYYY HH:mm:ss' }),
+        levelFormat(),
+        format.colorize(),
+        customFormat
+    ),
     transports: [new VorpalTransport()],
+    exceptionHandlers: [new winston.transports.File({ filename: 'exceptions.log' })],
 });
--- src/winston/vorpalTransport.ts
+++ src/winston/vorpalTransport.ts
@@ -1,5 +1,6 @@
 import TransportStream from 'winston-transport';
 import { vorpal } from '../cli/vorpal';
+import { MESSAGE } from 'triple-beam';
 
 export class VorpalTransport extends TransportStream {
     constructor(opts?: TransportStream.TransportStreamOptions) {
@@ -20,7 +21,8 @@ export class VorpalTransport extends TransportStream {
         });
 
         // Perform the writing to the remote service
-        vorpal.log(info.message);
+        const { level, message, ...meta } = info;
+        vorpal.log(info[MESSAGE]);
 
         callback();
     }
