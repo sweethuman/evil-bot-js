76f7363d5272920ab9e1c031b479d37c9d21fb47
sweethuman <hackscomicon@yahoo.com>
2019-10-25 14:30:43 +0300

    Add ts-events and Event in userMonitor when Users are Updated

--- src/bot/modules/userMonitor.ts
+++ src/bot/modules/userMonitor.ts
@@ -2,10 +2,12 @@ import { auth, firestore } from '../../firebase';
 import { logger } from '../../winston';
 import QuerySnapshot = firebase.firestore.QuerySnapshot;
 import firebase from 'firebase';
+import { SyncEvent } from 'ts-events';
 
 // TODO add proper typing for twitchUsers
 // tslint:disable-next-line:no-any
 export const twitchUsers: Map<string, TwitchDatabaseUser> = new Map<string, TwitchDatabaseUser>();
+export const usersUpdated = new SyncEvent<TwitchDatabaseUser[]>();
 
 export interface TwitchDatabaseUser {
     id: string;
@@ -24,6 +26,7 @@ export function load() {
         .onSnapshot(
             (ref: QuerySnapshot) => {
                 logger.debug('User Update Received');
+                const updates: TwitchDatabaseUser[] = [];
                 ref.docChanges().forEach(change => {
                     const { doc, type } = change;
                     const dataBaseUser: TwitchDatabaseUser = {
@@ -35,12 +38,15 @@ export function load() {
                     };
                     if (type === 'added') {
                         twitchUsers.set(doc.id, dataBaseUser);
+                        updates.push(dataBaseUser);
                     } else if (type === 'modified') {
                         twitchUsers.set(doc.id, dataBaseUser);
+                        updates.push(dataBaseUser);
                     } else if (type === 'removed') {
                         twitchUsers.delete(doc.id);
                     }
                 });
+                usersUpdated.post(updates);
             },
             error => logger.error(error.message)
         );
